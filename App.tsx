f4a48009f8a9c7496581ebd3c63ff0ab994a8b1b
brunolimaff-jpg <brunolimaff@gmail.com>
2026-02-20 16:56:06 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -191,7 +191,6 @@ function collectFullReport(messages: any[]): { text: string; sections: string[];
   }
 
   const fullText = sections.join('\n\n');
-
   return { text: fullText, sections, allLinks };
 }
 
