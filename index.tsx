b7014eae48f21beeaeb5a2bcc6c4a4fbaf2d2b8b
brunolimaff-jpg <brunolimaff@gmail.com>
2026-02-24 12:10:47 -0400

    Integrate CRMProvider, basic mini-CRM Kanban (CRMPipeline) and floating controls into App. Restore full App logic and add toggle between chat and CRM, plus 'Salvar sessão no CRM' using CRM context.

--- index.tsx
+++ index.tsx
@@ -1,9 +1,9 @@
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import { ModeProvider } from './contexts/ModeContext';
+import { CRMProvider } from './contexts/CRMContext';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -15,7 +15,9 @@ root.render(
   <React.StrictMode>
     <AuthProvider>
       <ModeProvider>
-        <App />
+        <CRMProvider>
+          <App />
+        </CRMProvider>
       </ModeProvider>
     </AuthProvider>
   </React.StrictMode>
